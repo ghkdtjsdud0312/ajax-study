1f7a80e82219e974cdd7d63aafc1c19f8eeffab8
ghkdtjsdud0312 <142463073+ghkdtjsdud0312@users.noreply.github.com>
2024-06-11 01:49:13 +0900

    6/10 ajax-ex-01내용추가중, ajaxViewController 추가

---
+++ src/main/java/ajax/ajax_study/controller/AjaxViewController.java
@@ -0,0 +1,12 @@
+package ajax.ajax_study.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AjaxViewController {
+    @GetMapping("/ajax-ex-01")
+    public String ajaxEx01() {
+        return "ajax-ex-01";
+    }
+}
--- src/main/java/ajax/ajax_study/controller/HomeController.java
+++ src/main/java/ajax/ajax_study/controller/HomeController.java
@@ -1,4 +1,4 @@
-package ajax.ajax_study;
+package ajax.ajax_study.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
