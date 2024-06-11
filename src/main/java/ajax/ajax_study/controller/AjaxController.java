15c0f493d3a94ea26228adddd340af70321b8118
ghkdtjsdud0312 <142463073+ghkdtjsdud0312@users.noreply.github.com>
2024-06-12 00:26:29 +0900

    6/12 AjaxController 내용추가

--- src/main/java/ajax/ajax_study/controller/AjaxController.java
+++ src/main/java/ajax/ajax_study/controller/AjaxController.java
@@ -2,12 +2,19 @@ package ajax.ajax_study.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class AjaxController {
-    @GetMapping("/ex01")
+
+    @GetMapping("/ajax-ex-01")
     public String ex01() {
         System.out.println("AjaxController.ex01");
-        return "index";
+        return "ajax-ex-01";
+        }
+    @PostMapping("/ajax-ex-02")
+    public String ex02() {
+        System.out.println("AjaxController.ex02");
+        return "ajax-ex-02";
     }
 }
