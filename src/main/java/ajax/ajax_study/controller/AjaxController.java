4c93cad99d149063b7f8bb913cc07037689bf313
ghkdtjsdud0312 <142463073+ghkdtjsdud0312@users.noreply.github.com>
2024-06-12 23:27:19 +0900

    6/12 AjaxController 내용추가

--- src/main/java/ajax/ajax_study/controller/AjaxController.java
+++ src/main/java/ajax/ajax_study/controller/AjaxController.java
@@ -3,6 +3,7 @@ package ajax.ajax_study.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -18,5 +19,13 @@ public class AjaxController {
     public @ResponseBody String ex02() {
         System.out.println("AjaxController.ex02");
         return "안녕하세요."; // 리턴 값이 그대로 출력됨
+    }
+
+    @GetMapping("/ex03")
+    // 파라미터 매개 변수 넣어 주기
+    public @ResponseBody String ex03(@RequestParam("param1") String param1,
+                                     @RequestParam("param2") String param2) {
+        System.out.println("param1 = " + param1 + ", param2 = " + param2);
+        return "ex03 메서드 호출 완료";
     }
 }
