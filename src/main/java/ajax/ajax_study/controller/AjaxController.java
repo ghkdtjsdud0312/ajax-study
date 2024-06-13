757da31d33f0a332c620fd0d2387ce5c8b939960
ghkdtjsdud0312 <142463073+ghkdtjsdud0312@users.noreply.github.com>
2024-06-13 19:33:34 +0900

    6/13 controller, ajax-ex-10.html 내용추가

--- src/main/java/ajax/ajax_study/controller/AjaxController.java
+++ src/main/java/ajax/ajax_study/controller/AjaxController.java
@@ -12,18 +12,21 @@ import java.util.List;
 @Controller
 public class AjaxController {
 
+    // 1. get 요청하기
     @GetMapping("/ex01")
     public String ex01() {
         System.out.println("AjaxController.ex01");
         return "index"; // index.html에 작성된 내용이 출력됨
         }
 
+    // 2. post 요청하기
     @PostMapping("/ex02")
     public @ResponseBody String ex02() {
         System.out.println("AjaxController.ex02");
         return "안녕하세요."; // 리턴 값이 그대로 출력됨
     }
 
+    // 3. get 요청 & parameter 전달하기
     @GetMapping("/ex03")
     // 파라미터 매개 변수 넣어 주기
     public @ResponseBody String ex03(@RequestParam("param1") String param1,
@@ -32,6 +35,7 @@ public class AjaxController {
         return "ex03 메서드 호출 완료";
     }
 
+    // 4. post 요청 & parameter 전달하기
     @PostMapping("/ex04")
     public @ResponseBody String ex04(@RequestParam("param1") String param1,
                                      @RequestParam("param2") String param2) {
@@ -39,6 +43,7 @@ public class AjaxController {
         return "ex04 메서드 호출 완료";
     }
 
+    // 5. get 요청 & parameter DTO 받기(@ModelAttribute) & DTO 객체 리턴 하기
     @GetMapping("/ex05")
     // @ResponseBody 뒤 AjaxDTO를 리턴 타입으로 지정하기
     public @ResponseBody AjaxDTO ex05(@ModelAttribute AjaxDTO ajaxDTO) {
@@ -46,12 +51,14 @@ public class AjaxController {
         return ajaxDTO; // html 파일에서 res 부분이 매개 값이 어떻게 보여지느냐를 확인 할 수 있다.
     }
 
+    // 6. post 요청 & parameter DTO 받기(@ModelAttribute) & DTO 객체 리턴 하기
     @PostMapping("/ex06")
     public @ResponseBody AjaxDTO ex06(@ModelAttribute AjaxDTO ajaxDTO) {
         System.out.println("ajaxDTO = " + ajaxDTO);
         return ajaxDTO;
     }
 
+    // 7. post 요청 & parameter DTO 받기(@RequestBody) & DTO 객체 리턴 하기
     @PostMapping("/ex07")
     // @ResponseBody : 응답을 어떻게 받을 수 있나
     // @RequestBody : 요청을 어떻게 받을 수 있나
@@ -60,6 +67,7 @@ public class AjaxController {
         return ajaxDTO;
     }
 
+    // 8. post 요청 & parameter 전달 하기 & List 객체 리턴 하기
     private List<AjaxDTO> DTOList() {
         List<AjaxDTO> dtoList = new ArrayList<>();
         dtoList.add(new AjaxDTO("data1", "data11")); // dtoList
@@ -77,6 +85,7 @@ public class AjaxController {
         return dtoList;
     }
 
+    // 9. post 요청 & parameter 전달 하기 & ResponseEntity에 DTO 객체 리턴 하기
     @PostMapping("/ex09")
     // @ResponseBody : 응답을 어떻게 받을 수 있나 / 데이터만 리턴을 준다 라는 의미
     // @RequestBody : 요청을 어떻게 받을 수 있나
@@ -84,7 +93,16 @@ public class AjaxController {
     public ResponseEntity ex09(@RequestBody AjaxDTO ajaxDTO) {
         System.out.println("ajaxDTO = " + ajaxDTO);
         return new ResponseEntity<>(HttpStatus.OK); // responseEntity를 여기서 선언한다. 여기서 데이터를 리턴하는 것이 아니라 스테이터스 코드만 리턴하는 경우만 리턴한다.
+     // return new ResponseEntity<>(HttpStatus.NOT_FOUND); // 요청처리에서 error가 났다라는 의미 -> 404에러 (실패가 뜸)
+     // return new ResponseEntity<>(ajaxDTO, HttpStatus.OK); // 데이터와 응답 코드를 같이 보내겠다
     }
 
-
+    // 9. post 요청 & parameter 전달 하기 & ResponseEntity에 List 객체 리턴 하기
+    @PostMapping("/ex10")
+        public ResponseEntity ex10(@RequestBody AjaxDTO ajaxDTO) {
+        System.out.println("ajaxDTO = " + ajaxDTO);
+        List<AjaxDTO> dtoList = DTOList();
+        dtoList.add(ajaxDTO); // 3개의 객체 리스트가 들어 있는 DTOList
+        return new ResponseEntity<>(dtoList, HttpStatus.OK); // 리스트 객체를 담아서 리턴
+    }
 }
--- src/main/java/ajax/ajax_study/controller/AjaxViewController.java
+++ src/main/java/ajax/ajax_study/controller/AjaxViewController.java
@@ -5,51 +5,62 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AjaxViewController {
+
+    // 1. get 요청하기
     @GetMapping("/ajax-ex-01")
     public String ajaxEx01() {
         return "ajax-ex-01";
     }
 
+    // 2. post 요청하기
     @GetMapping("/ajax-ex-02")
     public String ajaxEx02() {
         return "ajax-ex-02";
     }
 
+    // 3. get 요청 & parameter 전달하기
     @GetMapping("/ajax-ex-03")
     public String ajaxEx03() {
         return "ajax-ex-03";
     }
 
+    // 4. post 요청 & parameter 전달하기
     @GetMapping("/ajax-ex-04")
     public String ajaxEx04() {
         return "ajax-ex-04";
     }
 
+    // 5. get 요청 & parameter DTO 받기(@ModelAttribute) & DTO 객체 리턴 하기
     @GetMapping("/ajax-ex-05")
     public String ajaxEx05() {
         return "ajax-ex-05";
     }
 
+    // 6. post 요청 & parameter DTO 받기(@ModelAttribute) & DTO 객체 리턴 하기
     @GetMapping("/ajax-ex-06")
     public String ajaxEx06() {
         return "ajax-ex-06";
     }
 
+    // 7. post 요청 & parameter DTO 받기(@RequestBody) & DTO 객체 리턴 하기
     @GetMapping("/ajax-ex-07")
     public String ajaxEx07() {
         return "ajax-ex-07";
     }
 
+    // 8. post 요청 & parameter 전달 하기 & List 객체 리턴 하기
     @GetMapping("/ajax-ex-08")
     public String ajaxEx08() {
         return "ajax-ex-08";
     }
 
+    // 9. post 요청 & parameter 전달 하기 & ResponseEntity에 DTO 객체 리턴 하기
     @GetMapping("/ajax-ex-09")
     public String ajaxEx09() {
         return "ajax-ex-09";
     }
 
+    // 9. post 요청 & parameter 전달 하기 & ResponseEntity에 List 객체 리턴 하기
     @GetMapping("/ajax-ex-10")
     public String ajaxEx10() {
         return "ajax-ex-10";
